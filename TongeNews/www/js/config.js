de68075e75ae49d05305234aa7342a67b2d78cd3
eyea <1573765968@qq.com>
2016-09-13 18:19:42 +0800

    add TongeNews projects practice

---
+++ TongeNews/www/js/config.js
@@ -0,0 +1,11 @@
+//在www/js文件夹中新建一个js文件，命名为config.js，
+//在index.html中引用该config.js，
+//就可以在controllers.js中对config.js里申明的对象、变量、函数等进行调用。
+
+var domain = "http://www.tngou.net/api"
+var imgUrl = "http://tnfs.tngou.net/image"
+//imgURL在controller.js中tab1ctrl开始引用
+
+var cache ={
+    user :"TongeNews"
+}
