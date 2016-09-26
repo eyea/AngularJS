ae55046b50972f0826691216605c8059ed57692a
eyea <1573765968@qq.com>
2016-09-26 11:10:42 +0800

    change IDE && form finish

--- angularPractise/表单form/js/index.js
+++ angularPractise/表单form/js/index.js
@@ -1,7 +1,24 @@
 angular.module('myApp',[])
 
 .controller('firstCtrl',['$scope',function($scope){
-	
+	$scope.hobbies = [
+        {
+            id: '1',
+            name: '游泳'
+        },
+        {
+            id: '2',
+            name: '旅游'
+        },
+        {
+            id: '3',
+            name: '登山'
+        },
+    ];
+
+    $scope.data = [
+
+    ]
 	
 	
 
