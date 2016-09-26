aa558f7ef5fdc1e3353f305d66a00515ab41ce88
eyea <1573765968@qq.com>
2016-09-26 15:33:37 +0800

    directive templatr && templateUrl && transclue

--- angularPractise/自定义指令/js/index.js
+++ angularPractise/自定义指令/js/index.js
@@ -1,10 +1,13 @@
-angular.module('myApp',[])
+var app = angular.module('myApp',[]);
 
-.controller('firstCtrl',['$scope',function($scope){
-	
-	
-	
-
-}])
-
-//自定义指令 指令的名称
+app
+    //自定义指令 指令的名称
+    .directive('hello',function () {
+        return{
+            restrict: 'AE',
+            transclude: true,
+            template: '测试<div ng-transclude></div>'
+            // templateUrl: 'hello.html',
+            // replace: true,
+        }
+    })
