df774e84cad4cbc86decf77164bda3ba9c7b0f25
eyea <1573765968@qq.com>
2016-09-26 11:50:24 +0800

    skip form 38 39 40

--- angularPractise/表单form/js/index.js
+++ angularPractise/表单form/js/index.js
@@ -16,6 +16,24 @@ angular.module('myApp',[])
         },
     ];
 
+    $scope.cities = [
+        {
+            name: "上海",
+            parent: 0,
+            id:1
+        },
+        {
+            name: "上海市",
+            parent: 1,
+            id:2
+        },
+        {
+            name: "徐汇区",
+            parent: 2,
+            id:8
+        }
+    ];
+
     $scope.data = [
 
     ]
